71e72f17435e47faaff5d3d8bdf6e65d89691a8e
Adecarlos <adecarlos.evangelista@gmail.com>
2023-01-08 20:29:21 -0300

    Command line Testes
    Mudei a versão do junit para rodar corretamente no command line

--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -1,15 +1,17 @@
 package tests;
 
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
 
+
 @RunWith(Karate.class)
+@KarateOptions(tags = "~@ignore")
 public class TestRunner {
 
 //	@BeforeClass
 //	public static void before() {
 //		System.setProperty("karate.env", "QA");
-//	}
+// 	}
 }
